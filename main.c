48bf05f525d81d89189f7ee1dc760e3d0c989e65
k <Kylel@email.arizona.edu>
2023-12-11 11:35:35 -0700

    working for all phase3_n.txt cases

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include<math.h> // do not forget to compile : gcc main.c -lm
 #define PI 3.141592654
 #define MAX_LEN 61
-
+#define MED_LEN 32
 typedef struct RobotState_struct{
 	double x;
 	double y;
@@ -15,11 +15,10 @@ typedef struct RobotControl_struct{
    double v;
    double w;
 }RobotControl;
-/*
+
 typedef struct RobotNode_struct{
-   char name[50];
-   RobotState f;
-   RobotState i;
+   double time;
+   RobotState s;
    struct RobotNode_struct *nextNodePtr;
 }RobotNode;
 
@@ -39,8 +38,7 @@ void FreeRobotList(RobotNode* head){
        free(tmp);
     }
     return;
-}*/
-
+}
 double X_k_1(double x_k, double T, double theta_k, double v_k, double w_k){
    if(fabs(w_k) < 0.000000001){
       return x_k + T*cos(theta_k)*v_k;
@@ -97,29 +95,32 @@ int main(int argv, char **argc){
    
    double T[5], x[5], y[5], theta[5], v[150], w[150], d[150], b1, b2, distances[5], K[5];
    int k=0, robotNum =0;
-   char name[5][30], buf1[10], buf2[30], *end1, *end2, pbuf1[MAX_LEN], pbuf2[MAX_LEN];
+   char name[5][30], buf1[10], buf2[30], *end1, *end2;
    RobotControl u;
    RobotState p[5], s[5];
    
-   FILE * infile, * outfile, *temp;
+   FILE * infile, *temp;
    char buf[MAX_LEN], inFileName[MAX_LEN][MAX_LEN], outFileName[MAX_LEN][MAX_LEN];
-   
+   char *sub_search;
    int i, line =1;
    d[0]=0;
    
-   if(argv < 2 ){
+   if(argv < 3 ){
       printf("Error: Not enough input arguments.");
       return 0;
    }
    for(robotNum=0; robotNum<argv-1; robotNum++){
       for(i=0; i<strlen(argc[robotNum+1]); i++){ inFileName[robotNum][i] =argc[robotNum+1][i];//strcpy
-      }inFileName[robotNum][i] = '\0';// ---end strcpy
+      }inFileName[robotNum][i] = '\0';// ---end strcpy*/
       genOutFileName(inFileName[robotNum], outFileName[robotNum]);
       temp = fopen(outFileName[robotNum],"w"); fclose(temp);
       infile = fopen(inFileName[robotNum], "r"); // -----file operations 
       line = 1;
       while(fgets(buf, MAX_LEN, infile) != NULL){
-         if(line==1){ strcpy(name[robotNum], buf); }
+         if(line==1){ strcpy(name[robotNum], buf); name[robotNum][strlen(name[robotNum])-1]='\0';
+            sub_search=strstr(name[robotNum], "robot");
+            if(sub_search!=NULL){strcpy(name[robotNum], &name[robotNum][6]);}
+         }
          if(line==2){ sscanf(buf, "%lf %lf %lf %lf", &T[robotNum], &x[robotNum], &y[robotNum], &theta[robotNum]);
             s[robotNum].x = x[robotNum];
             s[robotNum].y = y[robotNum];
@@ -151,9 +152,11 @@ int main(int argv, char **argc){
       
       
    }
-  
+   for(i=0; i<robotNum; i++){
+      if(T[0]!=T[i]){ printf("Error: Sampling time must match.");return 0; }
+   }
    //print all robots
-   printf("Number of robots: %d\n", robotNum);
+   //printf("Number of robots: %d\n", robotNum);
    for(i=0;i<robotNum; i++){
       if(K[i]>10){
          PrintRobot(name[i], distances[i], y[i], x[i], s[i].x, s[i].y, T[i]*K[i]);
@@ -164,50 +167,48 @@ int main(int argv, char **argc){
    }
    
    //Make sure the time steps are the same
-   for(i=0; i<robotNum; i++){
-      if(T[0]!=T[i]){ printf("Error: Sampling time must match.");break; }
-   }
-   // TODO CREATE THESE FUNCTIONS which give max dist between robots and min dist between robots
+   if(robotNum >1){
+   
+   //  max dist between robots and min dist between robots
    FILE *FILE1 = fopen(outFileName[0], "r");
    FILE *FILE2 = fopen(outFileName[1], "r");
-   double x1, y1, x2, y2, t81, t82, t1, t2;
-   char * comma;
-   double max,tmax, min, tmin;
+   double x1, y1, x2, y2, t1, t2, t, trash;
+   
+   double max,tmax, min, tmin, new_distance;
+   RobotState r1, r2;
+   int num_args1,num_args2;
    int j=0;
-   while(fgets(pbuf1, MAX_LEN, FILE1)!=NULL && fgets(pbuf2, MAX_LEN, FILE2)!=NULL){
-      
-      pbuf1[7] = '\0'; (pbuf1[8]=='-')? (pbuf1[15] = '\0') : (pbuf1[14] = '\0'); pbuf1[24] = '\0';
-      comma = strchr(&pbuf1[17], ','); if(comma != NULL){ *comma = '\0';}
-      sscanf(&pbuf1[16], "%lf", &y1);
-      sscanf(pbuf1, "%lf", &t1);
-      
-      sscanf(&pbuf1[8], "%lf", &x1);
-      pbuf2[7] = '\0'; (pbuf2[8]=='-')? (pbuf2[15] = '\0') : (pbuf2[14] = '\0'); pbuf2[24] = '\0';
-      comma = strchr(&pbuf2[17], ','); if(comma != NULL){ *comma = '\0'; }
-      sscanf(&pbuf2[16], "%lf", &y2);
-      sscanf(pbuf2, "%lf", &t2);
-      
-      sscanf(&pbuf2[8], "%lf", &x2);
-      //printf("%lf: %lf %lf \n",t1, x1, y1);
-      //printf("%lf: %lf %lf \n", t2, x2, y2);
+   do{
+      num_args1=fscanf(FILE1, "%lf: %lf, %lf, %lf", &t1, &x1, &y1, &trash);
+      num_args2=fscanf(FILE2, "%lf: %lf, %lf, %lf", &t2, &x2, &y2, &trash);
+      if(num_args1==4){t = t1;r1.x =x1;r1.y =y1;}
+      if(num_args2==4){t = t2;r2.x =x2;r2.y =y2;}
+      //printf("%.4lf: %.4lf %.4lf \n", t1, r1.x, r1.y);
+      //printf("                      %.4lf: %.4lf %.4lf \n", t2, r2.x, r2.y);
       if(j==0){
-         max = sqrt(sq(x1 - x2)+sq(y1 - y2));
+         
+         max = distance(r1, r2);
          min = max;
          tmax =0;
          tmin =0;}
       else{
-         if(sqrt(sq(x1 - x2)+sq(y1 - y2)) - max >0.001){ max = sqrt(sq(x1 - x2)+sq(y1 - y2));tmax = t1;}
-         if(sqrt(sq(x1 - x2)+sq(y1 - y2)) - min <0.001){ min = sqrt(sq(x1 - x2)+sq(y1 - y2));tmin = t2;}
+         new_distance =distance(r1, r2);
+         if(new_distance - max >0.000001){ max = new_distance;tmax = t; }
+         if(new_distance - min <-0.000001){ min = new_distance;tmin = t; }
       }j++;
       
-   }
+   }while(num_args1 ==4||num_args2==4);
    fclose(FILE1);
    fclose(FILE2);
    //printf("max %lf @ %lf min %lf @ %lf\n", max,tmax, min,tmin);
-   printf("Max distance is %.4lf meters at time %.4lf seconds.\n",max,tmax);
+   if(tmax>0.1){tmax-=0.1000;}
+   if(tmin>0.1){tmin-=0.1000;}
+   printf("\nMax distance is %.4lf meters at time %.4lf seconds.\n",max,tmax);
    printf("Min distance is %.4lf meters at time %.4lf seconds.\n",min,tmin);
+   
+   }
    return 0;
-  
+   
    
 }
 
@@ -222,13 +223,23 @@ double distance(RobotState s1, RobotState s2){
    return d;
 }
 
+char* last_period(char *s){
+   int n = strlen(s), i;
+   for(i=n; i>0; i--){
+      if(s[i] == '.'){return &s[i];}
+   }
+   return NULL;
+}
+
+
 void genOutFileName(const char *infile, char *outfile){
    char * period_pointer, in_buf[MAX_LEN]="", out_buf[MAX_LEN]="", *i;
    int j=0;
    strcpy(in_buf, infile);
-   if(strchr(infile, '.')!=NULL){
+   if(last_period(in_buf)!=NULL){
       //printf("  has ext!\n");
-      period_pointer = strchr(in_buf, '.');
+      
+      period_pointer = last_period(in_buf);
       for(i=in_buf; i!=period_pointer; i++){
          out_buf[j] = *i;
          j++;
